25e54e407573ce77c2f296851b34859e7ae7d7bd
mehedirobi <mehedirobidev@gmail.com>
2026-06-26 19:34:04 +0600

    updated

--- index.js
+++ index.js
@@ -121,13 +121,27 @@ async function run() {
       res.send(result);
     })
 
-    app.get('product/bids/:productId', async (req, res) => {
-      const productId = req.params.productId;
-      const query = {product: productId}
-      const cursor = bidsCollection.find(query).sort({bid_price: -1})
-      const result = await cursor.toArray()
-      res.send(result)
+    app.delete("/bids/:id" , async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await bidsCollection.deleteOne(query)
+      res.send(result);
     })
+
+    app.get('/products/bids/:productId', async (req, res) => {
+  const productId = req.params.productId;
+
+  const query = {
+    product: productId,
+  };
+
+  const result = await bidsCollection
+    .find(query)
+    .sort({ bid_price: -1 })
+    .toArray();
+
+  res.send(result);
+});
 
 
     app.post("/bids", async (req, res) => {
